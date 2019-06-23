6abe7c8285b6bae730bbf4b8fe43b63e30772ec8
Shusuke Kusao <m4entacenti1@gmail.com>
2019-06-24 07:32:04 +0900

    Update template.py
    stdin.readline() is better for the purpose here
    https://stackoverflow.com/a/22624726

--- templates/template.py
+++ templates/template.py
@@ -1,19 +1,23 @@
 # run on python 3.7.2
+from sys import stdin
+
 
 def main():
-    n = int(input())
-    s = input()
+    n = int(stdin.readline())
+    s = stdin.readline()
     x = [None] * n
     y = [None] * n
     for i in range(n):
-        x[i], y[i] = map(int, input().split())
+        x[i], y[i] = map(int, stdin.readline().split())
     # solve here
     print(n)
     print(s)
     print(x[0])
     print(y[n - 1])
 
+
 def solve(n):
     return 0
+
 
 main()
